c0ab5ba5a43352e73fe7a209836fec0567641116
tysmitty418 <106200959+tysmitty418@users.noreply.github.com>
2022-05-24 17:43:48 -0500

    Add files via upload

---
+++ Be Kind (spyder file).py
@@ -0,0 +1,10 @@
+activeThings=('Go for a walk.' , 'Take the boys to the dog park.', 'Go for a bike ride.' , 'Go on a picnic.', 'Go over your support plan',)
+chillThings=('Cuddle and watch tv.' , 'Meditate.', 'Color.', 'Go to calm place.', 'Complete a chart of your support plan and decide where your located on it.', 'Fill out a paper of your support group', 'Go over your safty plan and write it down.', 'Help Tyson make his iPad cool.')
+from random import choice
+print("Are you feeling okay today?")
+yes=activeThings
+no=chillThings
+input
+print(choice(activeThings))
+
+  
